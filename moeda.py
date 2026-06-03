1ba301e87dc41cbd38057ec7dd5b7114a94e1608
Aniele Rodrigues <anielerodrigues19@gmail.com>
2026-06-03 17:14:05 -0300

    Add files via upload

---
+++ moeda.py
@@ -0,0 +1,9 @@
+pesos= int(input('How much do you have left in pesos? '))
+soles= int(input('How much do you have left in soles? '))
+reais= int(input('How much do you have left in reais? '))
+
+
+total= pesos * 17.34 + soles * 3.40 + reais * 5.09
+
+
+print(total)            # output: total in dollars
